8c0f10dcd919739e2f325ca8f73a5d0ca3fc3922
crishe07 <124082370+crishe07@users.noreply.github.com>
2023-02-02 09:25:23 +0100

    Add files via upload

---
+++ 0_procesos.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 09:16:23 2023
+
+@author: alumno
+"""
+
+from time import sleep
+from random import random
+
+from multiprocessing import Process
+
+def f():
+    for i in range(5):
+        print ("hola",i)
+        sleep(random())
+        
+if __name__=="__main__":
+    p=Process(target=f)
+    q=Process(target=f)
+    p.start()
+    q.start()
+    print("fin")
